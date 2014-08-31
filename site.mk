0a7eadf9cef4f0fab9755cda5d586da87376772b
root <root@static.74.113.40.188.clients.your-server.de>
2014-08-31 20:40:51 +0200

    missing packets :(

--- site.mk
+++ site.mk
@@ -1,11 +1,13 @@
 GLUON_SITE_PACKAGES := \
 	gluon-alfred \
 	gluon-autoupdater \
+	gluon-announced \
 	gluon-config-mode \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
 	gluon-next-node \
 	gluon-mesh-batman-adv \
 	gluon-mesh-vpn-fastd \

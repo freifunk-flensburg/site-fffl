50c08cbdb3e4d8679904d9ea58938b5c623c778a
DO9XE <h.l.lueth@gmail.com>
2014-08-31 13:57:17 +0200

    Site-files for the soon-to-be stable-version of our Gluon software

--- site.mk
+++ site.mk
@@ -1,32 +1,28 @@
 GLUON_SITE_PACKAGES := \
-	gluon-mesh-batman-adv-14 \
 	gluon-alfred \
 	gluon-autoupdater \
-	gluon-config-mode-autoupdater \
-	gluon-config-mode-hostname \
-	gluon-config-mode-mesh-vpn \
-	gluon-config-mode-geo-location \
-	gluon-config-mode-contact-info \
+	gluon-config-mode \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-legacy \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
 	gluon-next-node \
+	gluon-mesh-batman-adv \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
 	gluon-status-page \
 	gluon-ath9k-workaround \
-	gluon-announced \
-	gluon-luci-portconfig \
 	iwinfo \
 	iptables \
 	haveged
 
 
-DEFAULT_GLUON_RELEASE := stable-$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := stable-14.3.0
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
 GLUON_PRIORITY ?= 0
+
+GLUON_BRANCH=stable

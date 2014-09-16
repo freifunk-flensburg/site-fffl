e7f9f562aa361f0619f03ef1b22cd83240ee15b6
Hendrik Lüth <H.L.Lueth@gmail.com>
2014-09-16 07:37:21 +0200

    Changed site.mk for gluon-14.4

--- site.mk
+++ site.mk
@@ -1,29 +1,34 @@
-GLUON_SITE_PACKAGES := \
-	gluon-alfred \
-	gluon-autoupdater \
-	gluon-announced \
-	gluon-config-mode \
-	gluon-ebtables-filter-multicast \
-	gluon-ebtables-filter-ra-dhcp \
-	gluon-luci-admin \
-	gluon-luci-autoupdater \
-	gluon-luci-portconfig \
-	gluon-next-node \
-	gluon-mesh-batman-adv \
-	gluon-mesh-vpn-fastd \
-	gluon-radvd \
-	gluon-status-page \
-	gluon-ath9k-workaround \
-	iwinfo \
-	iptables \
-	haveged
+GLUON_SITE_PACKAGES := \ 
+	gluon-mesh-batman-adv-15 \ 
+	gluon-alfred \ 
+	gluon-autoupdater \ 
+	gluon-config-mode-autoupdater \ 
+	gluon-config-mode-hostname \ 
+	gluon-config-mode-mesh-vpn \ 
+	gluon-config-mode-geo-location \ 
+	gluon-config-mode-contact-info \ 
+	gluon-ebtables-filter-multicast \ 
+	gluon-ebtables-filter-ra-dhcp \ 
+	gluon-luci-admin \ 
+	gluon-luci-autoupdater \ 
+	gluon-next-node \ 
+	gluon-mesh-vpn-fastd \ 
+	gluon-radvd \ 
+	gluon-status-page \ 
+	gluon-ath9k-workaround \ 
+	gluon-announced \ 
+	gluon-luci-portconfig \ 
+	iwinfo \ 
+	iptables \ 
+	haveged 
 
+ 
+DEFAULT_GLUON_RELEASE := beta-14.4.0 
 
-DEFAULT_GLUON_RELEASE := stable-14.3.0
-
-# Allow overriding the release number from the command line
-GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+ 
+# Allow overriding the release number from the command line 
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE) 
 
 GLUON_PRIORITY ?= 0
 
-GLUON_BRANCH=stable
+GLUON_BRANCH=beta

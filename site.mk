7497e939a8d26a3623ad56817eaeb966bbc6164a
Sven Thomsen <sventhomsen@foni.net>
2015-06-12 19:06:13 +0200

    changed DEFAULT_GLUON_RELEASE to 2015.1.0-0 changed GLUON_SITE_PACKAGES added gluon-mesh-batman-adv-15 removed gluon-mesh-batman-adv-14 added gluon-config-mode-core removed gluon-luci-private-wifi added gluon-luci-wifi-config added gluon-setup-mode (warning not buildable)

--- site.mk
+++ site.mk
@@ -1,5 +1,5 @@
 GLUON_SITE_PACKAGES := \
-	gluon-mesh-batman-adv-14 \
+	gluon-mesh-batman-adv-15 \
 	gluon-alfred \
 	gluon-announced \
 	gluon-autoupdater \
@@ -8,21 +8,23 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-mesh-vpn \
 	gluon-config-mode-geo-location \
 	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
 	gluon-luci-portconfig \
-	gluon-luci-private-wifi \
+	gluon-luci-wifi-config\
 	gluon-next-node \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
+	gluon-setup-mode \
 	gluon-status-page \
 	iwinfo \
 	iptables \
 	haveged
 
-DEFAULT_GLUON_RELEASE := stable-2014.4.0-0
+DEFAULT_GLUON_RELEASE := stable-2015.1.0-0
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
 GLUON_PRIORITY ?= 0

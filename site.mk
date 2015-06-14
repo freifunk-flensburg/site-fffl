e56af5cb58989ca0ffd3aa32a5118649a174f785
Sven Thomsen <sventhomsen@foni.net>
2015-06-14 17:39:24 +0200

    changed branch and revision to beta made it compatible with new features of Gluon 15.1 changed batman back to batman14 to allow test of the routers in current network fixed multi language support

--- site.mk
+++ site.mk
@@ -1,33 +1,40 @@
 GLUON_SITE_PACKAGES := \
-	gluon-mesh-batman-adv-15 \
+	gluon-mesh-batman-adv-14 \
 	gluon-alfred \
 	gluon-announced \
 	gluon-autoupdater \
-	gluon-config-mode-hostname \
+	gluon-setup-mode \
+	gluon-config-mode-core \
 	gluon-config-mode-autoupdater \
+	gluon-config-mode-hostname \
 	gluon-config-mode-mesh-vpn \
 	gluon-config-mode-geo-location \
 	gluon-config-mode-contact-info \
-	gluon-config-mode-core \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
 	gluon-luci-portconfig \
+	gluon-luci-private-wifi \
 	gluon-luci-wifi-config\
 	gluon-next-node \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
-	gluon-setup-mode \
 	gluon-status-page \
 	iwinfo \
 	iptables \
 	haveged
 
-DEFAULT_GLUON_RELEASE := stable-2015.1.0-0
+DEFAULT_GLUON_RELEASE := 2015.1.0-beta-$(shell date '+%Y-%m-%d_%H.%M')
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
 GLUON_PRIORITY ?= 0
 
-DEFAULT_GLUON_BRANCH = stable
+DEFAULT_GLUON_BRANCH = beta
 GLUON_BRANCH ?= $(DEFAULT_GLUON_BRANCH)
+export GLUON_BRANCH
+
+GLUON_TARGET ?= ar71xx-generic
+export GLUON_TARGET
+
+GLUON_LANGS ?= en de da

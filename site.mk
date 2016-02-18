2f9fdf17ca18c17ae9a55f4c6b05acb11e3caa85
horstfffl <horsthorsthorst@hmamail.com>
2016-02-18 18:58:53 +0100

    changed release_name to "exp-2016.1.0-0_11s_mesh"

--- site.mk
+++ site.mk
@@ -25,7 +25,7 @@ GLUON_SITE_PACKAGES := \
 	iptables \
 	haveged
 
-DEFAULT_GLUON_RELEASE := 2016.1.0-exp-11s-$(shell date '+%m-%d_%H_%M')
+DEFAULT_GLUON_RELEASE := exp-2016.1.0-0_11s_mesh
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
 

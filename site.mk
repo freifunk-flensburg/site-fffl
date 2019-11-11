7b15f3f3a9e033e80e569130b814e7f55a3e3e7d
horstfffl <horsthorsthorst@hmamail.com>
2019-11-11 09:55:01 +0100

    add GLUON_DEPRECATED := full to site.mk

--- site.mk
+++ site.mk
@@ -34,6 +34,7 @@ export GLUON_TARGET
 #ath10k devices cant mesh on "ibss" and "11s" at the same time so one needs to be choosen or images wont be build. 
 GLUON_WLAN_MESH ?= 11s
 #"some devices contain a region code that restricts firmware installations. Set GLUON_REGION to eu or us to make the resulting images installable from the respective stock firmwares." from readthedocs gluon
+GLUON_DEPRECATED := full
+# full build(factory and sysupgrade) of depricated devices, other options: (0;upgrade)
 GLUON_REGION ?= eu
-
 GLUON_LANGS ?= en de da

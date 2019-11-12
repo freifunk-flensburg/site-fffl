9322ef5d1b916399e1fe78bba23f1080e48b99a7
horstfffl <horsthorsthorst@hmamail.com>
2019-11-12 22:45:04 +0100

    fix typo in site.mk

--- site.mk
+++ site.mk
@@ -2,7 +2,7 @@ GLUON_FEATURES := \
 	alfred \
 	autoupdater \
 	config-mode-geo-location-osm \
-	config-mode-domain-select
+	config-mode-domain-select \
 	web-private-wifi \
 	ebtables-filter-multicast \
 	ebtables-filter-ra-dhcp \
